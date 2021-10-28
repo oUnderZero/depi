88edbbbb5f17430bb7240d00297faea7af932a16
jaz-Ealeen <73973313+jaz-Ealeen@users.noreply.github.com>
2021-10-27 20:53:27 -0500

    modificacion

---
+++ scripts/form-carousel_5d9d.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'depi@tecnm.morelia.mx',
+    'to' => 'elsanguinario3257@gmail.com'
+    ),
+    'fields' => array(
+    'email' => array(
+    'order' => 1,
+    'type' => 'email',
+    'label' => 'email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'email\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
